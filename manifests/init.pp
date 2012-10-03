875372c003bf80b34018f59eaae8db372c0b139f
Tim Sharpe <tim@github.com>
2012-10-03 15:15:47 -0700

    tomdoc colloquy class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,8 @@
+# Public: Install Colloquy.app to /Applications.
+#
+# Examples
+#
+#   include colloquy
 class colloquy {
   package { 'Colloquy':
     source   => 'http://colloquy.info/downloads/colloquy-latest.zip',
